c9e4a0c9984c9940fc0d59a70e80bbdfad2dfbec
Antoni Saputra <antonisaputra049@gmail.com>
2023-09-28 18:42:01 +0700

    init project with nuxt, primevue, tailwindcss, pinia

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: 'src/',
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/main.css',
+    'primevue/resources/themes/lara-light-blue/theme.css',
+    'primeicons/primeicons.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  imports: {
+    dirs: ['stores', 'stores/**']
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+  ],
+  pinia: {
+    autoImports: [
+      'defineStore',
+      'storeToRefs'
+    ]
+  },
+  build: {
+    transpile: ['primevue']
+  }
+})
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="flex flex-col gap-4 w-full h-screen justify-center items-center">
+        <h1>HomePage</h1>
+        <Button label="Increment" icon="pi pi-check" @click="increment" />
+        <span>{{ count }}</span>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '~/stores';
+
+const store = useCounterStore()
+
+const { name, doubleCount, count } = storeToRefs(store)
+const { increment } = store
+</script>
