e8575ec89ddc5faf9ead1814ee388b2144849578
“Andi <andpermana1998@gmail.com>
2023-10-01 20:09:24 +0700

    fix: change folder directory for components

--- src/components/LiquidLoading.vue
+++ src/components/LiquidLoading.vue
@@ -1,11 +1,10 @@
 <template>
-<div class="w-full fixed h-screen z-40 top-0 px-3 flex justify-center items-center bg-black/10 backdrop-blur-sm">
-  <Dialog :visible="true" :closable="false" :modal="true" :show-header="false" :unstyled="true">
-    <div class="custom-loader"></div>
-  </Dialog>
-</div>
+  <div class="w-full fixed h-screen z-40 top-0 px-3 flex justify-center items-center bg-black/10 backdrop-blur-sm">
+    <Dialog :visible="true" :closable="false" :modal="true" :show-header="false" :unstyled="true">
+      <div class="custom-loader" />
+    </Dialog>
+  </div>
 </template>
-
 
 <style>
 .custom-loader {
@@ -14,14 +13,14 @@
     display: grid;
 }
 .custom-loader::before,
-.custom-loader::after {    
+.custom-loader::after {
     content:"";
     grid-area: 1/1;
     --c: radial-gradient(farthest-side,#9628f0 92%,#0000);
-    background: 
-      var(--c) 50%  0, 
-      var(--c) 50%  100%, 
-      var(--c) 100% 50%, 
+    background:
+      var(--c) 50%  0,
+      var(--c) 50%  100%,
+      var(--c) 100% 50%,
       var(--c) 0    50%;
     background-size: 12px 12px;
     background-repeat: no-repeat;
@@ -34,7 +33,7 @@
   animation-timing-function: linear
 }
 
-@keyframes s2{ 
+@keyframes s2{
   100%{transform: rotate(.5turn)}
 }
 </style>
