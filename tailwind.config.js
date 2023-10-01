dad9b59c79163ec42ef90db8f9913d6668932899
Antoni Saputra <antonisaputra049@gmail.com>
2023-10-01 11:08:26 +0700

    feat: add colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: '#161525',
+        secondary: '#1D1E33',
+        darkSecondary: '#19182A',
+        darkPrimary: '#12111E',
+        neutral: '#242333'
       },
       fontFamily: {
       },
