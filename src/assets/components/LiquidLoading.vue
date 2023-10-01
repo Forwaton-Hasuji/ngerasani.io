6ab85a9e888b2fff150152946d4e3557301bc216
“Andi <andpermana1998@gmail.com>
2023-10-01 12:18:19 +0700

    feat: create loading component with css.gif is time waster

---
+++ src/assets/components/LiquidLoading.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="w-full fixed h-screen z-40 top-0 px-3 flex justify-center items-center bg-black/10 backdrop-blur-sm">
+  <Dialog :visible="true" :closable="false" :modal="true" :show-header="false" :unstyled="true">
+    <div class="custom-loader"></div>
+  </Dialog>
+</div>
+</template>
+
+
+<style>
+.custom-loader {
+    width: 50px;
+    height: 50px;
+    display: grid;
+}
+.custom-loader::before,
+.custom-loader::after {    
+    content:"";
+    grid-area: 1/1;
+    --c: radial-gradient(farthest-side,#9628f0 92%,#0000);
+    background: 
+      var(--c) 50%  0, 
+      var(--c) 50%  100%, 
+      var(--c) 100% 50%, 
+      var(--c) 0    50%;
+    background-size: 12px 12px;
+    background-repeat: no-repeat;
+    animation: s2 2s infinite;
+}
+.custom-loader::before {
+  margin:4px;
+  filter:hue-rotate(45deg);
+  background-size: 8px 8px;
+  animation-timing-function: linear
+}
+
+@keyframes s2{ 
+  100%{transform: rotate(.5turn)}
+}
+</style>
--- src/plugins/primevue.ts
+++ src/plugins/primevue.ts
@@ -3,13 +3,15 @@ import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import Toast from 'primevue/toast'
 import ToastService from 'primevue/toastservice'
+import Dialog from 'primevue/dialog'
 import '@/assets/css/main.css'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(PrimeVue, { ripple: true })
-  nuxtApp.vueApp.use(ToastService)
-  nuxtApp.vueApp.component('Button', Button)
-  nuxtApp.vueApp.component('InputText', InputText)
-  nuxtApp.vueApp.component('Toast', Toast)
-  // other components that you need
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true, unstyled: true})
+    nuxtApp.vueApp.use(ToastService)
+    nuxtApp.vueApp.component('Button', Button)
+    nuxtApp.vueApp.component('InputText', InputText)
+    nuxtApp.vueApp.component('Toast', Toast)
+    nuxtApp.vueApp.component('Dialog', Dialog)
+    //other components that you need
 })
