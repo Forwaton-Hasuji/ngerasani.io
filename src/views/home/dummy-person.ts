e31052c3fd2d25473462db3f49e8d34cc7d0bb0c
forest <adityaforestresananta@gmail.com>
2023-10-05 02:41:15 +0700

    feat: ui home desktop

---
+++ src/views/home/dummy-person.ts
@@ -0,0 +1,229 @@
+const personList = [
+  {
+    name: 'Eca Aura',
+    age: 20,
+    rating: 4.9,
+    ratingCount: 51300,
+    job: 'Entertainer / Host',
+    gender: 'Female',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Aditya Forest Resananta',
+    age: 26,
+    rating: 5.0,
+    ratingCount: 72012,
+    job: 'Alumni Refactory',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Anthony Saputra',
+    age: 20,
+    rating: 3.0,
+    ratingCount: 123,
+    job: 'Petani Pisang',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Andi Permana',
+    age: 31,
+    rating: 1.5,
+    ratingCount: 2700,
+    job: 'Buddha Kelima',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Susmana',
+    age: 40,
+    rating: 3.1,
+    ratingCount: 1234,
+    job: 'Bapak ETA Indonesia',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Eca Aura',
+    age: 20,
+    rating: 4.9,
+    ratingCount: 51300,
+    job: 'Entertainer / Host',
+    gender: 'Female',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Aditya Forest Resananta',
+    age: 26,
+    rating: 5.0,
+    ratingCount: 72012,
+    job: 'Alumni Refactory',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Anthony Saputra',
+    age: 20,
+    rating: 3.0,
+    ratingCount: 123,
+    job: 'Petani Pisang',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Andi Permana',
+    age: 31,
+    rating: 1.5,
+    ratingCount: 2700,
+    job: 'Buddha Kelima',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Susmana',
+    age: 40,
+    rating: 3.1,
+    ratingCount: 1234,
+    job: 'Bapak ETA Indonesia',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Eca Aura',
+    age: 20,
+    rating: 4.9,
+    ratingCount: 51300,
+    job: 'Entertainer / Host',
+    gender: 'Female',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Aditya Forest Resananta',
+    age: 26,
+    rating: 5.0,
+    ratingCount: 72012,
+    job: 'Alumni Refactory',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Anthony Saputra',
+    age: 20,
+    rating: 3.0,
+    ratingCount: 123,
+    job: 'Petani Pisang',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Andi Permana',
+    age: 31,
+    rating: 1.5,
+    ratingCount: 2700,
+    job: 'Buddha Kelima',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Susmana',
+    age: 40,
+    rating: 3.1,
+    ratingCount: 1234,
+    job: 'Bapak ETA Indonesia',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Eca Aura',
+    age: 20,
+    rating: 4.9,
+    ratingCount: 51300,
+    job: 'Entertainer / Host',
+    gender: 'Female',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Aditya Forest Resananta',
+    age: 26,
+    rating: 5.0,
+    ratingCount: 72012,
+    job: 'Alumni Refactory',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Anthony Saputra',
+    age: 20,
+    rating: 3.0,
+    ratingCount: 123,
+    job: 'Petani Pisang',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Andi Permana',
+    age: 31,
+    rating: 1.5,
+    ratingCount: 2700,
+    job: 'Buddha Kelima',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Susmana',
+    age: 40,
+    rating: 3.1,
+    ratingCount: 1234,
+    job: 'Bapak ETA Indonesia',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Eca Aura',
+    age: 20,
+    rating: 4.9,
+    ratingCount: 51300,
+    job: 'Entertainer / Host',
+    gender: 'Female',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Aditya Forest Resananta',
+    age: 26,
+    rating: 5.0,
+    ratingCount: 72012,
+    job: 'Alumni Refactory',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Anthony Saputra',
+    age: 20,
+    rating: 3.0,
+    ratingCount: 123,
+    job: 'Petani Pisang',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Andi Permana',
+    age: 31,
+    rating: 1.5,
+    ratingCount: 2700,
+    job: 'Buddha Kelima',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  },
+  {
+    name: 'Susmana',
+    age: 40,
+    rating: 3.1,
+    ratingCount: 1234,
+    job: 'Bapak ETA Indonesia',
+    gender: 'Male',
+    photoPath: 'images/dummy-photo.jpg'
+  }
+]
+
+export default personList
